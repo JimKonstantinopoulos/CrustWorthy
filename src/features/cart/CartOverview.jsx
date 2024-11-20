a2304c23acdb5a073d1b8f01e4b508ec89b321c8
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2024-11-20 20:00:17 +0200

    Implement Redux functionality in CartOverview to display quantity of pizzas and total price

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartQuantity, getTotalCartSum } from "./cartSlice";
+import { formatCurrency } from "../../utilities/helpers";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartSum = useSelector(getTotalCartSum);
+
+  if (!totalCartQuantity) return null;
+
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 text-sm uppercase text-stone-200 tablet:p-6 tablet_w:text-base">
       <p className="space-x-4 font-semibold text-stone-300 tablet:space-x-5">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartSum)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
